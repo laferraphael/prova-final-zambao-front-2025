f8578f1dd1110c7ba466298d0acb3a4645713924
laferraphael <laferraphael@gmail.com>
2025-11-10 10:14:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PessoasApp from './Pessoa'
+import FilmeApp from './Filme'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <PessoasApp></PessoasApp>
+        <FilmeApp></FilmeApp>
       </div>
     </>
   )
