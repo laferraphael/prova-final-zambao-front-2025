c9538561ee4807464c876720ac8bc867edf33d08
laferraphael <laferraphael@gmail.com>
2025-11-10 10:21:47 -0300

    Create LogoutButton.jsx

---
+++ src/LogoutButton.jsx
@@ -0,0 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function LogoutButton() {
+  const { logout } = useAuth0();
+
+  return (
+    <button
+      onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+      className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+    >
+      Sair
+    </button>
+  );
+}
+
